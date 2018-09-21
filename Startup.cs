8f5375c682f2536ab413238fc43750689cf1c97a
yichengtsai <yichengtsai@JC1106029>
2018-09-21 10:14:36 +0800

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mod04_01.Startup))]
+namespace Mod04_01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
